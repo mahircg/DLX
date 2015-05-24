05594429225cad9e3a2bc809bd14abdabbe8f263
Mahircan Gul <guelm@rhrk.uni-kl.de>
2015-05-24 22:53:11 +0200

    Final working version of decode stage added

--- DLX/src/dlx_pipe_id.vhd
+++ DLX/src/dlx_pipe_id.vhd
@@ -156,10 +156,123 @@ begin
     imm26( 6 to 31)  := unsigned(if_id_ir_imm26);
     
     case if_id_ir_opcode is
-      -- ====================================================
-      -- Your code for combinatorial decoder logic goes here
-      -- ====================================================    
-      
+      when op_j =>
+		id_npc 					<= if_id_npc + imm26;
+		id_cond					<= '1'; 
+		id_opcode_class_int  <= NOFORW;
+		
+		when op_jal =>
+		id_npc 					<= if_id_npc + imm26;
+		id_cond					<= '1';
+		id_opcode_class_int  <= NOFORW;
+		
+		when op_jalr =>
+		id_npc					<= unsigned(id_a_fwd);
+		id_cond					<= '1';
+		id_opcode_class_int		<= BRANCH;
+		
+		when op_jr	=>
+		id_npc					<= unsigned(id_a_fwd);
+		id_cond					<= '1';
+		id_opcode_class_int		<= BRANCH;
+		
+		when op_beqz =>
+		if(id_a_fwd = X"00000000") then
+			id_npc					<= if_id_npc + imm16;
+			id_cond					<= '1';
+			id_opcode_class_int  <= BRANCH;
+		else
+			NULL;
+		end if;
+		
+		when op_bnez =>
+		if(id_a_fwd /= X"00000000") then
+			id_npc					<= if_id_npc + imm16;
+			id_cond					<= '1';
+			id_opcode_class_int  <= BRANCH;
+		else
+			NULL;
+		end if;
+		
+		when op_trap =>
+			id_opcode_class_int		<= NOFORW;
+			--At this stage,traps are not handled.Therefore system is terminated whenever an exception occurs.
+			--id_npc					<= if_id_npc  + std_logic_vector(resize(unsigned(imm26), id_npc'length));
+			--id_cond					<= '1';
+			id_halt <= '1';
+		
+		when op_special =>
+			id_opcode_class_int		<= RR_ALU;
+			
+		when op_lb =>			
+			id_opcode_class_int		<= LOAD;
+			
+		when op_lbu =>
+			id_opcode_class_int		<= LOAD;
+		
+		when op_lh =>
+			id_opcode_class_int		<= LOAD;
+		
+		when op_lhi =>
+			id_opcode_class_int		<= LOAD;
+		
+		when op_lhu =>
+			id_opcode_class_int		<= LOAD;
+		
+		when op_lw =>
+			id_opcode_class_int		<= LOAD;
+				
+		when op_sb =>
+			id_opcode_class_int		<= STORE;
+			
+		when op_sh =>
+			id_opcode_class_int		<= STORE;
+			
+		when op_addi =>
+			id_opcode_class_int		<= IM_ALU;
+			
+		when op_addui =>
+			id_opcode_class_int 		<= IM_ALU;
+			
+		when op_andi =>
+			id_opcode_class_int		<= IM_ALU;
+			
+		when op_ori =>
+			id_opcode_class_int		<= IM_ALU;
+		
+		when op_seqi =>
+			id_opcode_class_int		<= IM_ALU;
+		
+		when op_sgei =>
+			id_opcode_class_int		<= IM_ALU;
+		
+		when op_sgti =>
+			id_opcode_class_int		<= IM_ALU;
+		
+		when op_slti =>
+			id_opcode_class_int		<= IM_ALU;
+		
+		when op_snei =>
+			id_opcode_class_int		<= IM_ALU;
+		
+		when op_srai =>
+			id_opcode_class_int		<= IM_ALU;
+		
+		when op_srli =>
+			id_opcode_class_int		<= IM_ALU;
+			
+		when op_slli =>
+			id_opcode_class_int		<= IM_ALU;
+		
+		when op_subi =>
+			id_opcode_class_int		<= IM_ALU;
+			
+		when op_subui =>
+			id_opcode_class_int		<= IM_ALU;
+		
+		when op_xori =>
+			id_opcode_class_int		<= IM_ALU;
+
       when others =>
         id_opcode_class_int <= NOFORW;
         id_illegal_instr    <= '1';
@@ -213,9 +326,258 @@ begin
 
             -- von NOP abweichendes Verhalten
             case if_id_ir_opcode is
-              -- ====================================================
-              -- Your code for sequential decoder logic goes here
-              -- ====================================================    
+              when op_j => 		NULL;
+				  
+				  when op_jal =>
+					id_ex_reg_rd  		<= REG_31;
+					id_ex_reg_wen		<= '1';
+					id_ex_a				<= std_logic_vector(if_id_npc + unsigned("000000" & if_id_ir_imm26));
+					id_ex_alu_func		<= alu_a;
+					
+					when op_jalr =>
+					id_ex_reg_rd  		<= REG_31;
+					id_ex_reg_wen		<= '1';
+					id_ex_a				<= std_logic_vector(if_id_npc + unsigned("000000" & if_id_ir_imm26));
+					id_ex_alu_func		<= alu_a;
+					
+					when op_jr	=>		NULL;
+					
+					when op_beqz =>	NULL;
+					
+					when op_bnez =>	NULL;
+					
+					when op_trap =>	NULL;
+					
+					when op_special =>
+						--id_ex_alu_func 	<= if_id_ir_spfunc;
+						id_ex_reg_rd		<=	if_id_ir_rd_rtype;
+						id_ex_reg_wen		<= '1';
+						case if_id_ir_spfunc is
+							when sp_add  =>
+								id_ex_alu_func		 <= alu_add;
+							when sp_and  =>
+								id_ex_alu_func		 <= alu_and;
+							when sp_or =>
+								id_ex_alu_func		 <= alu_or;
+							when sp_seq =>
+								id_ex_alu_func		 <= alu_seq;
+							when sp_sge =>
+								id_ex_alu_func		 <= alu_sge;
+							when sp_sgt =>
+								id_ex_alu_func		 <= alu_sgt;
+							when sp_sle =>
+								id_ex_alu_func		 <= alu_sle;
+							when sp_slt =>
+								id_ex_alu_func		 <= alu_slt;
+							when sp_sne =>
+								id_ex_alu_func		 <= alu_sne;
+							when sp_sll =>
+								id_ex_alu_func		 <= alu_sll;
+							when sp_sra =>
+								id_ex_alu_func		 <= alu_sra;
+							when sp_srl =>
+								id_ex_alu_func		 <= alu_srl;
+							when sp_sub =>
+								id_ex_alu_func		 <= alu_sub;
+							when sp_xor =>
+								id_ex_alu_func		 <= alu_xor;
+							when sp_addu =>
+								id_ex_alu_func		 <= alu_add;
+								id_ex_us_sel       <= SEL_UNSIGNED;
+							when sp_subu =>
+								id_ex_alu_func		 <= alu_sub;
+								id_ex_us_sel		 <= SEL_UNSIGNED;
+							when others =>			 NULL;
+						end case;
+					
+					when op_lb =>
+						id_ex_data_sel			<= SEL_DM_OUT;
+						id_ex_dm_en				<= '1';
+						id_ex_reg_rd 			<= if_id_ir_rd_itype;
+						id_ex_reg_wen			<= '1';
+						id_ex_alu_opb_sel 	<= SEL_ID_EX_IMM;
+						id_ex_dm_width			<= MEM_WIDTH_BYTE;
+						id_ex_us_sel       	<= SEL_SIGNED;
+						id_ex_alu_func			<= alu_add;
+						
+					when op_lbu =>
+						id_ex_data_sel			<= SEL_DM_OUT;
+						id_ex_dm_en				<= '1';
+						id_ex_reg_rd 			<= if_id_ir_rd_itype;
+						id_ex_reg_wen			<= '1';
+						id_ex_alu_opb_sel 	<= SEL_ID_EX_IMM;
+						id_ex_dm_width			<= MEM_WIDTH_BYTE;
+						id_ex_us_sel       	<= SEL_SIGNED;
+						id_ex_alu_func			<= alu_add;
+					
+					when op_lh =>
+						id_ex_data_sel			<= SEL_DM_OUT;
+						id_ex_dm_en				<= '1';
+						id_ex_reg_rd 			<= if_id_ir_rd_itype;
+						id_ex_reg_wen			<= '1';
+						id_ex_alu_opb_sel 	<= SEL_ID_EX_IMM;
+						id_ex_dm_width			<= MEM_WIDTH_HALFWORD;
+						id_ex_us_sel       	<= SEL_SIGNED;
+						id_ex_alu_func			<= alu_add;
+						
+					when op_lhi =>
+						
+						id_ex_reg_rd			<= if_id_ir_rd_itype;
+						id_ex_reg_wen			<= '1';
+						id_ex_a					<= if_id_ir_imm16 & X"0000";
+						id_ex_alu_func			<= alu_a;
+						
+					when op_lhu =>
+						id_ex_data_sel			<= SEL_DM_OUT;
+						id_ex_dm_en				<= '1';
+						id_ex_reg_rd 			<= if_id_ir_rd_itype;
+						id_ex_reg_wen			<= '1';
+						id_ex_alu_opb_sel 	<= SEL_ID_EX_IMM;
+						id_ex_dm_width			<= MEM_WIDTH_HALFWORD;
+						id_ex_us_sel       	<= SEL_SIGNED;
+						id_ex_alu_func			<= alu_add;	
+					
+					when op_lw =>
+						id_ex_data_sel			<= SEL_DM_OUT;
+						id_ex_dm_en				<= '1';
+						id_ex_reg_rd 			<= if_id_ir_rd_itype;
+						id_ex_reg_wen			<= '1';
+						id_ex_alu_opb_sel 	<= SEL_ID_EX_IMM;
+						id_ex_dm_width			<= MEM_WIDTH_WORD;
+						id_ex_us_sel       	<= SEL_SIGNED;
+						id_ex_alu_func			<= alu_add;	
+						
+					when op_sb =>
+						id_ex_dm_en				<= '1';
+						id_ex_dm_wen			<= '1';
+						id_ex_reg_rd			<= if_id_ir_rd_itype;
+						id_ex_alu_opb_sel 	<= SEL_ID_EX_IMM;
+						id_ex_dm_width			<= MEM_WIDTH_BYTE;
+						id_ex_us_sel       	<= SEL_SIGNED;
+						id_ex_alu_func			<= alu_add;	
+						
+					when op_sh =>
+						id_ex_dm_en				<= '1';
+						id_ex_dm_wen			<= '1';
+						id_ex_reg_rd			<= if_id_ir_rd_itype;
+						id_ex_alu_opb_sel 	<= SEL_ID_EX_IMM;
+						id_ex_dm_width			<= MEM_WIDTH_HALFWORD;
+						id_ex_us_sel       	<= SEL_SIGNED;
+						id_ex_alu_func			<= alu_add;		
+					
+					when op_addi =>
+						id_ex_alu_opb_sel 	<= SEL_ID_EX_IMM;
+						id_ex_reg_rd			<= if_id_ir_rd_itype;
+						id_ex_reg_wen			<= '1';
+						id_ex_us_sel       	<= SEL_SIGNED;
+						id_ex_alu_func			<= alu_add;
+						
+					when op_addui =>
+						id_ex_alu_opb_sel 	<= SEL_ID_EX_IMM;
+						id_ex_reg_rd			<= if_id_ir_rd_itype;
+						id_ex_reg_wen			<= '1';
+						id_ex_us_sel       	<= SEL_UNSIGNED;
+						id_ex_alu_func			<= alu_add;
+						id_ex_imm				<= '0' & if_id_ir_imm16;
+						
+					when op_andi =>
+						id_ex_alu_opb_sel 	<= SEL_ID_EX_IMM;
+						id_ex_reg_rd			<= if_id_ir_rd_itype;
+						id_ex_reg_wen			<= '1';
+						id_ex_us_sel       	<= SEL_UNSIGNED;
+						id_ex_alu_func			<= alu_and;
+						id_ex_imm				<= '0' & if_id_ir_imm16;
+						
+					when op_ori =>
+						id_ex_alu_opb_sel 	<= SEL_ID_EX_IMM;
+						id_ex_reg_rd			<= if_id_ir_rd_itype;
+						id_ex_reg_wen			<= '1';
+						id_ex_us_sel       	<= SEL_UNSIGNED;
+						id_ex_alu_func			<= alu_or;
+						id_ex_imm				<= '0' & if_id_ir_imm16;
+						
+						
+					when op_seqi =>
+						id_ex_alu_opb_sel 	<= SEL_ID_EX_IMM;
+						id_ex_reg_rd			<= if_id_ir_rd_itype;
+						id_ex_reg_wen			<= '1';
+						id_ex_us_sel       	<= SEL_UNSIGNED;
+						id_ex_alu_func			<= alu_seq;
+					
+					when op_sgei =>
+						id_ex_alu_opb_sel 	<= SEL_ID_EX_IMM;
+						id_ex_reg_rd			<= if_id_ir_rd_itype;
+						id_ex_reg_wen			<= '1';
+						id_ex_us_sel       	<= SEL_UNSIGNED;
+						id_ex_alu_func			<= alu_sge;	
+					
+					when op_sgti =>
+						id_ex_alu_opb_sel 	<= SEL_ID_EX_IMM;
+						id_ex_reg_rd			<= if_id_ir_rd_itype;
+						id_ex_reg_wen			<= '1';
+						id_ex_us_sel       	<= SEL_UNSIGNED;
+						id_ex_alu_func			<= alu_sgt;	
+						
+					when op_slti =>
+						id_ex_alu_opb_sel 	<= SEL_ID_EX_IMM;
+						id_ex_reg_rd			<= if_id_ir_rd_itype;
+						id_ex_reg_wen			<= '1';
+						id_ex_us_sel       	<= SEL_UNSIGNED;
+						id_ex_alu_func			<= alu_slt;	
+						
+					when op_snei =>
+						id_ex_alu_opb_sel 	<= SEL_ID_EX_IMM;
+						id_ex_reg_rd			<= if_id_ir_rd_itype;
+						id_ex_reg_wen			<= '1';
+						id_ex_us_sel       	<= SEL_UNSIGNED;
+						id_ex_alu_func			<= alu_sne;	
+					
+					when op_srai =>
+						id_ex_alu_opb_sel 	<= SEL_ID_EX_IMM;
+						id_ex_reg_rd			<= if_id_ir_rd_itype;
+						id_ex_reg_wen			<= '1';
+						id_ex_us_sel       	<= SEL_UNSIGNED;
+						id_ex_alu_func			<= alu_sra;
+						id_ex_imm				<= '0' & if_id_ir_imm16;
+						
+					when op_srli =>
+						id_ex_alu_opb_sel 	<= SEL_ID_EX_IMM;
+						id_ex_reg_rd			<= if_id_ir_rd_itype;
+						id_ex_reg_wen			<= '1';
+						id_ex_us_sel       	<= SEL_UNSIGNED;
+						id_ex_alu_func			<= alu_srl;
+						id_ex_imm				<= '0' & if_id_ir_imm16; 
+						
+					when op_slli =>
+						id_ex_alu_opb_sel 	<= SEL_ID_EX_IMM;
+						id_ex_reg_rd			<= if_id_ir_rd_itype;
+						id_ex_reg_wen			<= '1';
+						id_ex_us_sel       	<= SEL_SIGNED;
+						id_ex_alu_func			<= alu_sll;
+						id_ex_imm				<= if_id_ir_imm16(0) & if_id_ir_imm16;
+					
+					when op_subi =>
+						id_ex_alu_opb_sel 	<= SEL_ID_EX_IMM;
+						id_ex_reg_rd			<= if_id_ir_rd_itype;
+						id_ex_reg_wen			<= '1';
+						id_ex_us_sel       	<= SEL_SIGNED;
+						id_ex_alu_func			<= alu_sub;	
+					
+					when op_subui =>
+						id_ex_alu_opb_sel 	<= SEL_ID_EX_IMM;
+						id_ex_reg_rd			<= if_id_ir_rd_itype;
+						id_ex_reg_wen			<= '1';
+						id_ex_us_sel       	<= SEL_UNSIGNED;
+						id_ex_alu_func			<= alu_sub;
+						id_ex_imm				<= '0' & if_id_ir_imm16;	
+					
+					when op_xori =>
+						id_ex_alu_opb_sel 	<= SEL_ID_EX_IMM;
+						id_ex_reg_rd			<= if_id_ir_rd_itype;
+						id_ex_reg_wen			<= '1';
+						id_ex_us_sel       	<= SEL_UNSIGNED;
+						id_ex_alu_func			<= alu_xor;
+						id_ex_imm				<= '0' & if_id_ir_imm16;
 
               when others => null;
                 -- Tue nichts. Illegal-Instruktion wird kombinatorisch
